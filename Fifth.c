6b1d30b905a2f65c1c8885fdeda067600e5fb362
Nikitae <siknorius@gmail.comm>
2023-07-09 14:03:52 +0400

    refactoring Fifth

--- Fifth.c
+++ Fifth.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 int main (void)
 {
-printf("Меня зовут Никита Чебуков\n");
-printf("Я мой адрес Ижевск");
-return 0;
+    printf("Меня зовут Никита Чебуков\n");
+    printf("Я мой адрес Ижевск");
+
+    return 0;
 }
