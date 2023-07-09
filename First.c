ce4001b82d89d8f6d1e44cba5063d29e2964fbf1
Nikitae <siknorius@gmail.comm>
2023-07-09 13:35:17 +0400

    Refactoring first

--- First.c
+++ First.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-printf("Чебуков Никита\n");
-printf("Никита\n");
-printf("Чебуков\n");
-printf("Чебуков Никита \n");
+    printf("Чебуков Никита\n");
+    printf("Никита\n"); 
+    printf("Чебуков\n");
+    printf("Чебуков Никита \n");
 
-return 0;
+    return 0;
 }
