27cca34f5844aaf8bcac484677dc74d66c82fc05
Nikitae <siknorius@gmail.comm>
2023-07-11 13:11:33 +0400

    My Thirteenth Project

--- Thirteenth.c
+++ Thirteenth.c
@@ -6,7 +6,9 @@ int years, seconds;
 
     printf("Your age:\n");
     scanf("%d", &years);
+    
     seconds = years * 3.156e7;
+    
     printf("Your live already more than %d seconds", seconds);
     getchar();getchar();
 
