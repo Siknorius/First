d11532f9a59d191a46b607d73ca11a2f43bf2d19
Nikitae <siknorius@gmail.comm>
2023-07-11 13:23:34 +0400

    My 14 Project

---
+++ Fourteenth.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+float quarts, grams, molecules;
+    
+    printf("How much water do you drink daily (in quarts):\n");
+    scanf("%f", &quarts);
+    
+    grams = quarts * 950;
+    molecules = grams / 3.0e-23;
+    
+    printf("You consume daily %e water molecules", molecules);
+    getchar();getchar();
+
+    return 0;
+}
