1b14651e32dcf9392574b010dcd7c6cc77423ea4
Nikitae <siknorius@gmail.comm>
2023-07-13 12:25:33 +0400

    18_project

---
+++ 4 chapter/18_project.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char name[20];
+
+    printf("Enter your name:\n");
+    scanf("%s", name);
+    printf("\"%s\"\n", name);
+    printf("\"%20s\"\n", name);
+    printf("\"%-20s\"\n", name);
+    printf("%*s", strlen(name) + 3, name);
+    getchar();
+    
+    return 0;
+}
